a31850efc5c6414330de721779c5887695272ab1
Simon Ekström <simon.ekstrom1@learnet.se>
2025-06-08 14:03:30 +0200

    second commit

--- src/assets/pages/EventDetails.jsx
+++ src/assets/pages/EventDetails.jsx
@@ -21,25 +21,27 @@ const EventDetails = () => {
 
     fetchEvent()
   }, [id])
+
+  if (!event) return null;
   
-    return (
+  return (
     <PortalLayout title="Event Details" onBack={() => navigate(-1)}>
-        <div class="event-container">
-            <div class="event-image"></div>
-            <div class="event-info-container">
-                <h4 class="event-title">{event.name}</h4>
+        <div className="event-container">
+            <div className="event-image"></div>
+            <div className="event-info-container">
+                <h4 className="event-title">{event.name}</h4>
                 <div>
-                    <div class="event-date">
+                    <div className="event-date">
                         <img src={CalendarIcon} alt="Date"/>
-                        <p class="text-body-11">{event.dateTime}</p>
-                        <p class="text-body-10">Tickets Sold</p>
-                        <p class="text-body-10">Starts from</p>
+                        <p className="text-body-11">{event.dateTime}</p>
+                        <p className="text-body-10">Tickets Sold</p>
+                        <p className="text-body-10">Starts from</p>
                     </div>
-                    <div class="event-location">
+                    <div className="event-location">
                         <img src={LocationIcon} alt=""/>
-                        <p class="text-body-11">{event.location}</p>
-                        <p class="text-body-16">xx.xxx</p>
-                        <p class="text-body-10">/xx.xxx</p>
+                        <p className="text-body-11">{event.location}</p>
+                        <p className="text-body-16">xx.xxx</p>
+                        <p className="text-body-10">/xx.xxx</p>
                         <h6>${event.price}</h6>
                     </div>
                 </div>
